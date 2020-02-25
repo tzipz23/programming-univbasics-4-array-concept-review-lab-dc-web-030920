aad572f157a10f9ca2481325b3a53ea9feb8fca3
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-25 00:55:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,8 @@ def find_element_index(array, value_to_find)
   array = [1,2,3]
   length = array.length
   
-  length.times do |2|
-  puts scale[2]
-  end
+  length.times do |index|
+  puts scale[index]
 end
 
 def find_max_value(array)
