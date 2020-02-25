c5c922f8917293122007116e3fb7221404f680fa
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-25 00:54:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,8 @@ def find_element_index(array, value_to_find)
   array = [1,2,3]
   length = array.length
   
-  scale.length.times { |2|
+  length.times do |2|
   puts scale[2]
-  }
 end
 
 def find_max_value(array)
